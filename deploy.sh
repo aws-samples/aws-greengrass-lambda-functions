64256d1f5dc8399ae72b6fecd7be24f5f43336ce
Tim Mattison <tim@mattison.org>
2018-12-06 10:22:45 -0500

    Added deploy.sh script for users that don't clone the aws-greengrass-provisioner repo

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+REGION=$(aws configure get region)
+
+if [ -z "$REGION" ]; then
+  echo The REGION variable is not set, cannot continue
+  exit 1
+fi
+
+PWD=$(pwd)
+
+docker pull timmattison/aws-greengrass-provisioner:master
+
+docker run \
+   -v $PWD/foundation:/foundation \
+   -v $PWD/deployments:/deployments \
+   -v $PWD/functions:/functions \
+   -v $PWD/credentials:/credentials \
+   -v $PWD/ggds:/ggds \
+   -v $PWD/build:/build \
+   -v $HOME/.ssh:/root/.ssh \
+   -it --rm \
+   -e AWS_REGION=$REGION \
+   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+   timmattison/aws-greengrass-provisioner:master $@
