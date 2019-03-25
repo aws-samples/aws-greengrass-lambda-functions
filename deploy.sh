8843a077ef16d71c8c369269d6c6742ff2301d3f
Tim Mattison <tim@mattison.org>
2019-03-25 13:22:16 -0400

    Forgot to push the session token into the container, this fixes invalid security token errors

--- deploy.sh
+++ deploy.sh
@@ -89,4 +89,5 @@ docker run \
    -e AWS_REGION=$REGION \
    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+   -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN \
    timmattison/aws-greengrass-provisioner:master $@
