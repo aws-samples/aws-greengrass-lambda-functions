0774f261f972c2bea56570e21030f6b116e8e9b2
Tim Mattison <tim@mattison.org>
2019-05-08 13:20:14 -0400

    Added some startup debug messages

--- functions/CDDSenseHatJava/src/main/java/com/amazonaws/greengrass/cddsensehat/handlers/StartupHandler.java
+++ functions/CDDSenseHatJava/src/main/java/com/amazonaws/greengrass/cddsensehat/handlers/StartupHandler.java
@@ -1,8 +1,10 @@
 package com.amazonaws.greengrass.cddsensehat.handlers;
 
+import com.amazonaws.greengrass.cddsensehat.data.Topics;
 import com.amazonaws.greengrass.cddsensehat.events.TimerFiredEvent;
 import com.google.common.eventbus.EventBus;
 import com.timmattison.greengrass.cdd.events.GreengrassStartEvent;
+import com.timmattison.greengrass.cdd.events.PublishMessageEvent;
 import com.timmattison.greengrass.cdd.handlers.interfaces.GreengrassStartEventHandler;
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +17,7 @@ public class StartupHandler implements GreengrassStartEventHandler {
     private final int DELAY_MS = 5000;
     private final int PERIOD_MS = 5000;
     private final EventBus eventBus;
+    private final Topics topics;
 
     @Override
     public void execute(GreengrassStartEvent greengrassStartEvent) {
@@ -25,5 +28,10 @@ public class StartupHandler implements GreengrassStartEventHandler {
                 eventBus.post(new TimerFiredEvent());
             }
         }, DELAY_MS, PERIOD_MS);
+
+        eventBus.post(PublishMessageEvent.builder().topic(topics.getDebugOutputTopic()).message("SenseHat started [" + System.currentTimeMillis() + "]").build());
+        eventBus.post(PublishMessageEvent.builder().topic(topics.getDebugOutputTopic()).message("List topic: " + topics.getListTopic()).build());
+        eventBus.post(PublishMessageEvent.builder().topic(topics.getDebugOutputTopic()).message("Start topic: " + topics.getStartTopic()).build());
+        eventBus.post(PublishMessageEvent.builder().topic(topics.getDebugOutputTopic()).message("Stop topic: " + topics.getStopTopic()).build());
     }
 }
