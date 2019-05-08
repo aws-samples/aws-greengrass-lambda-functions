8b7ec294e7b123207080d9a6f75586ac0ba1e1f2
Tim Mattison <tim@mattison.org>
2019-05-08 13:19:53 -0400

    Removed unused Vaadin code

--- functions/CDDSenseHatJava/src/main/java/com/amazonaws/greengrass/cddsensehat/handlers/StartupHandler.java
+++ functions/CDDSenseHatJava/src/main/java/com/amazonaws/greengrass/cddsensehat/handlers/StartupHandler.java
@@ -1,7 +1,6 @@
 package com.amazonaws.greengrass.cddsensehat.handlers;
 
 import com.amazonaws.greengrass.cddsensehat.events.TimerFiredEvent;
-import com.amazonaws.greengrass.cddsensehat.vaadin.EmbeddedVaadin;
 import com.google.common.eventbus.EventBus;
 import com.timmattison.greengrass.cdd.events.GreengrassStartEvent;
 import com.timmattison.greengrass.cdd.handlers.interfaces.GreengrassStartEventHandler;
@@ -15,13 +14,10 @@ import java.util.TimerTask;
 public class StartupHandler implements GreengrassStartEventHandler {
     private final int DELAY_MS = 5000;
     private final int PERIOD_MS = 5000;
-    private final EmbeddedVaadin embeddedVaadin;
     private final EventBus eventBus;
 
     @Override
     public void execute(GreengrassStartEvent greengrassStartEvent) {
-        embeddedVaadin.start();
-
         Timer timer = new Timer(true);
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
