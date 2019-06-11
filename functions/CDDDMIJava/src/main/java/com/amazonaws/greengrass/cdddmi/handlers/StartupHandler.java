6a8c900050612a7953b758359ebbf16def56b243
Tim Mattison <tim@mattison.org>
2019-06-11 16:36:53 -0400

    Manually delomboked DMI function

--- functions/CDDDMIJava/src/main/java/com/amazonaws/greengrass/cdddmi/handlers/StartupHandler.java
+++ functions/CDDDMIJava/src/main/java/com/amazonaws/greengrass/cdddmi/handlers/StartupHandler.java
@@ -1,18 +1,22 @@
 package com.amazonaws.greengrass.cdddmi.handlers;
 
 import com.amazonaws.greengrass.cdddmi.data.Topics;
+import com.awslabs.aws.iot.greengrass.cdd.events.GreengrassStartEvent;
+import com.awslabs.aws.iot.greengrass.cdd.events.ImmutablePublishMessageEvent;
+import com.awslabs.aws.iot.greengrass.cdd.handlers.interfaces.GreengrassStartEventHandler;
 import com.google.common.eventbus.EventBus;
-import com.timmattison.greengrass.cdd.events.GreengrassStartEvent;
-import com.timmattison.greengrass.cdd.events.PublishMessageEvent;
-import com.timmattison.greengrass.cdd.handlers.interfaces.GreengrassStartEventHandler;
-import lombok.RequiredArgsConstructor;
 
 import javax.inject.Inject;
 
-@RequiredArgsConstructor(onConstructor = @__(@Inject))
 public class StartupHandler implements GreengrassStartEventHandler {
-    private final EventBus eventBus;
-    private final Topics topics;
+    @Inject
+    EventBus eventBus;
+    @Inject
+    Topics topics;
+
+    @Inject
+    public StartupHandler() {
+    }
 
     /**
      * Receives the Greengrass start event from the event bus, publishes a message indicating it has started, and creates
@@ -22,6 +26,6 @@ public class StartupHandler implements GreengrassStartEventHandler {
      */
     @Override
     public void execute(GreengrassStartEvent greengrassStartEvent) {
-        eventBus.post(PublishMessageEvent.builder().topic(topics.getOutputTopic()).message("DMI started [" + System.currentTimeMillis() + "]").build());
+        eventBus.post(ImmutablePublishMessageEvent.builder().topic(topics.getOutputTopic()).message("DMI started [" + System.currentTimeMillis() + "]").build());
     }
 }
