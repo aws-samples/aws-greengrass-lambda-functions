459550d9b7b84bc97d6998db38cd390c0fa9b91c
Tim Mattison <tim@mattison.org>
2019-12-05 06:45:55 -0500

    Updates for baseline 0.5.11

--- foundation/CDDBaselineJava/src/main/java/com/awslabs/aws/iot/greengrass/cdd/helpers/JsonHelper.java
+++ foundation/CDDBaselineJava/src/main/java/com/awslabs/aws/iot/greengrass/cdd/helpers/JsonHelper.java
@@ -1,9 +1,15 @@
 package com.awslabs.aws.iot.greengrass.cdd.helpers;
 
+import com.awslabs.aws.iot.greengrass.cdd.events.GreengrassLambdaEvent;
+
+import java.util.Optional;
+
 public interface JsonHelper {
     String toJson(Object object);
 
     <T> T fromJson(Class<T> clazz, byte[] json);
 
     <T> T fromJson(Class<T> clazz, String json);
+
+    <T> Optional<T> fromJson(Class<T> clazz, GreengrassLambdaEvent greengrassLambdaEvent);
 }
--- foundation/CDDBaselineJava/src/main/java/com/awslabs/aws/iot/greengrass/cdd/helpers/implementations/BasicJsonHelper.java
+++ foundation/CDDBaselineJava/src/main/java/com/awslabs/aws/iot/greengrass/cdd/helpers/implementations/BasicJsonHelper.java
@@ -1,13 +1,20 @@
 package com.awslabs.aws.iot.greengrass.cdd.helpers.implementations;
 
+import com.awslabs.aws.iot.greengrass.cdd.events.GreengrassLambdaEvent;
 import com.awslabs.aws.iot.greengrass.cdd.helpers.JsonHelper;
 import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapterFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.util.Map;
+import java.util.Optional;
 import java.util.ServiceLoader;
 
 public class BasicJsonHelper implements JsonHelper {
+    private final Logger log = LoggerFactory.getLogger(BasicJsonHelper.class);
+
     @Inject
     public BasicJsonHelper() {
     }
@@ -41,5 +48,27 @@ public class BasicJsonHelper implements JsonHelper {
     public <T> T fromJson(Class<T> clazz, String json) {
         return getGsonBuilder().create()
                 .fromJson(json, clazz);
+    }
+
+    @Override
+    public <T> Optional<T> fromJson(Class<T> clazz, GreengrassLambdaEvent greengrassLambdaEvent) {
+        if (!greengrassLambdaEvent.getJsonInput().isPresent()) {
+            log.error("No JSON payload present for conversion to a [" + clazz.getName() + "]");
+            return Optional.empty();
+        }
+
+        // Get the JSON input as a map
+        Map input = greengrassLambdaEvent.getJsonInput().get();
+
+        // Convert it back to a JSON string
+        String jsonString = toJson(input);
+
+        try {
+            // Convert the JSON string to the requested class
+            return Optional.ofNullable(fromJson(clazz, jsonString));
+        } catch (Exception e) {
+            log.error("Could not convert JSON [" + jsonString + "], to a [" + clazz.getName() + "]");
+            return Optional.empty();
+        }
     }
 }
