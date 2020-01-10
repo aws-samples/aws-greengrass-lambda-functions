6fabc1cd018d584b322bd9d67059424b0cd516ed
Tim Mattison <tim@mattison.org>
2020-01-10 15:10:03 -0500

    ggp.sh reformatting

--- ggp.sh
+++ ggp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TAG=`cd ../aws-greengrass-provisioner 2> /dev/null && git symbolic-ref --short HEAD | tr -cd '[:alnum:]._-'`
+TAG=$(cd ../aws-greengrass-provisioner 2>/dev/null && git symbolic-ref --short HEAD | tr -cd '[:alnum:]._-')
 
 if [ $? -ne 0 ]; then
   TAG=master
@@ -18,19 +18,19 @@ set +e
 CLI=1
 DOCKER_SESSION_TOKEN=""
 
-hash aws 2> /dev/null
+hash aws 2>/dev/null
 
 if [ $? -ne 0 ]; then
   echo "AWS CLI not installed, looking for credentials via instance metadata service (EC2 only)"
   CLI=0
 else
-# Is the AWS CLI configured?
+  # Is the AWS CLI configured?
   AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
 fi
 
 if [ $? -ne 0 ] || [ $CLI -eq 0 ]; then
   # AWS CLI is not configured.  Are we running on an EC2 instance with a role?
-  ROLE=`curl -s --connect-timeout 3 http://169.254.169.254/latest/meta-data/iam/security-credentials/`
+  ROLE=$(curl -s --connect-timeout 3 http://169.254.169.254/latest/meta-data/iam/security-credentials/)
 
   if [ $? -ne 0 ]; then
     echo $AWS_CLI_ERROR_MESSAGE_PREFIX access key ID $AWS_CLI_ERROR_MESSAGE_SUFFIX
@@ -42,18 +42,18 @@ if [ $? -ne 0 ] || [ $CLI -eq 0 ]; then
     fi
 
     # Looks like an EC2 instance with a role, get the credentials
-    CREDENTIALS=`curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/$ROLE`
+    CREDENTIALS=$(curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/$ROLE)
 
     # Extract the credentials
-    AWS_ACCESS_KEY_ID=`echo $CREDENTIALS | sed -e 's/^.*AccessKeyId\" : \"\([^\"]*\).*/\1/'`
-    AWS_SECRET_ACCESS_KEY=`echo $CREDENTIALS | sed -e 's/^.*SecretAccessKey\" : \"\([^\"]*\).*/\1/'`
-    AWS_SESSION_TOKEN=`echo $CREDENTIALS | sed -e 's/^.*Token\" : \"\([^\"]*\).*/\1/'`
+    AWS_ACCESS_KEY_ID=$(echo $CREDENTIALS | sed -e 's/^.*AccessKeyId\" : \"\([^\"]*\).*/\1/')
+    AWS_SECRET_ACCESS_KEY=$(echo $CREDENTIALS | sed -e 's/^.*SecretAccessKey\" : \"\([^\"]*\).*/\1/')
+    AWS_SESSION_TOKEN=$(echo $CREDENTIALS | sed -e 's/^.*Token\" : \"\([^\"]*\).*/\1/')
     DOCKER_SESSION_TOKEN="-e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN"
 
     # Get the region
-    AVAILABILITY_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
-    REGION="`echo \"$AVAILABILITY_ZONE\" | sed 's/[a-z]$//'`"
-  fi 
+    AVAILABILITY_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+    REGION="$(echo \"$AVAILABILITY_ZONE\" | sed 's/[a-z]$//')"
+  fi
 else
   AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
 
@@ -70,9 +70,8 @@ else
   fi
 fi
 
-
 # Simple check to make sure docker is installed
-hash docker 2> /dev/null
+hash docker 2>/dev/null
 
 if [ $? -ne 0 ]; then
   echo "Docker may not be installed on your system. If you received a permission denied error try running this script again as root. Otherwise, please install docker and try again."
@@ -86,7 +85,7 @@ PWD=$(pwd)
 
 docker pull timmattison/aws-greengrass-provisioner:$TAG
 
-TEMP_CONTAINER=`docker create timmattison/aws-greengrass-provisioner:$TAG`
+TEMP_CONTAINER=$(docker create timmattison/aws-greengrass-provisioner:$TAG)
 
 docker cp $PWD/foundation $TEMP_CONTAINER:/foundation
 docker cp $PWD/deployments $TEMP_CONTAINER:/deployments
@@ -94,21 +93,21 @@ docker cp $PWD/functions $TEMP_CONTAINER:/functions
 docker cp $PWD/ggds $TEMP_CONTAINER:/ggds
 docker cp $PWD/connectors $TEMP_CONTAINER:/connectors
 
-TEMP_IMAGE=`uuidgen | tr '[:upper:]' '[:lower:]' | tr -d '-'`
+TEMP_IMAGE=$(uuidgen | tr '[:upper:]' '[:lower:]' | tr -d '-')
 
 docker commit $TEMP_CONTAINER $TEMP_IMAGE
 docker rm $TEMP_CONTAINER
 
 docker run \
-   -v $PWD/credentials:/credentials \
-   -v $PWD/build:/build \
-   -v $HOME/.ssh:/root/.ssh \
-   -v $PWD/dtoutput:/dtoutput \
-   -v $PWD/logs:/logs \
-   -v /var/run/docker.sock:/var/run/docker.sock \
-   -i --rm \
-   -e AWS_REGION=$REGION \
-   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-   $DOCKER_SESSION_TOKEN \
-   $TEMP_IMAGE $@
+  -v $PWD/credentials:/credentials \
+  -v $PWD/build:/build \
+  -v $HOME/.ssh:/root/.ssh \
+  -v $PWD/dtoutput:/dtoutput \
+  -v $PWD/logs:/logs \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -i --rm \
+  -e AWS_REGION=$REGION \
+  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  $DOCKER_SESSION_TOKEN \
+  $TEMP_IMAGE $@
