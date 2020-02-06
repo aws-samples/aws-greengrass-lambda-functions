f493ab6c40836d39e2ea5e03dd6f4070e80fe0d5
Tim Mattison <tim@mattison.org>
2020-02-06 16:59:02 -0500

    Updated CDD DMI to use new publish methods

--- functions/CDDDMIJava/src/main/java/com/amazonaws/greengrass/cdddmi/handlers/InputHandler.java
+++ functions/CDDDMIJava/src/main/java/com/amazonaws/greengrass/cdddmi/handlers/InputHandler.java
@@ -2,20 +2,19 @@ package com.amazonaws.greengrass.cdddmi.handlers;
 
 import com.amazonaws.greengrass.cdddmi.data.Topics;
 import com.amazonaws.greengrass.cdddmi.dmi.interfaces.DmiFetcher;
+import com.awslabs.aws.iot.greengrass.cdd.communication.Communication;
 import com.awslabs.aws.iot.greengrass.cdd.events.GreengrassLambdaEvent;
-import com.awslabs.aws.iot.greengrass.cdd.events.ImmutablePublishObjectEvent;
 import com.awslabs.aws.iot.greengrass.cdd.handlers.interfaces.GreengrassLambdaEventHandler;
-import com.google.common.eventbus.EventBus;
 
 import javax.inject.Inject;
 
 public class InputHandler implements GreengrassLambdaEventHandler {
     @Inject
-    EventBus eventBus;
-    @Inject
     DmiFetcher dmiFetcher;
     @Inject
     Topics topics;
+    @Inject
+    Communication communication;
 
     @Inject
     public InputHandler() {
@@ -28,6 +27,6 @@ public class InputHandler implements GreengrassLambdaEventHandler {
 
     @Override
     public void execute(GreengrassLambdaEvent greengrassLambdaEvent) {
-        eventBus.post(ImmutablePublishObjectEvent.builder().topic(topics.getOutputTopic()).object(dmiFetcher.fetch().get()).build());
+        dmiFetcher.fetch().ifPresent(data -> communication.publishObjectEvent(topics.getOutputTopic(), data));
     }
 }
--- functions/CDDDMIJava/src/main/java/com/amazonaws/greengrass/cdddmi/handlers/StartupHandler.java
+++ functions/CDDDMIJava/src/main/java/com/amazonaws/greengrass/cdddmi/handlers/StartupHandler.java
@@ -1,18 +1,17 @@
 package com.amazonaws.greengrass.cdddmi.handlers;
 
 import com.amazonaws.greengrass.cdddmi.data.Topics;
+import com.awslabs.aws.iot.greengrass.cdd.communication.Communication;
 import com.awslabs.aws.iot.greengrass.cdd.events.GreengrassStartEvent;
-import com.awslabs.aws.iot.greengrass.cdd.events.ImmutablePublishMessageEvent;
 import com.awslabs.aws.iot.greengrass.cdd.handlers.interfaces.GreengrassStartEventHandler;
-import com.google.common.eventbus.EventBus;
 
 import javax.inject.Inject;
 
 public class StartupHandler implements GreengrassStartEventHandler {
     @Inject
-    EventBus eventBus;
-    @Inject
     Topics topics;
+    @Inject
+    Communication communication;
 
     @Inject
     public StartupHandler() {
@@ -26,6 +25,6 @@ public class StartupHandler implements GreengrassStartEventHandler {
      */
     @Override
     public void execute(GreengrassStartEvent greengrassStartEvent) {
-        eventBus.post(ImmutablePublishMessageEvent.builder().topic(topics.getOutputTopic()).message("DMI started [" + System.currentTimeMillis() + "]").build());
+        communication.publishMessageEvent(topics.getOutputTopic(), "DMI started [" + System.currentTimeMillis() + "]");
     }
 }
