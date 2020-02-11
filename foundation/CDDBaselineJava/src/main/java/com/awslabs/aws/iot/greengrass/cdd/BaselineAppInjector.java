1ae72a28ae33374833d73f3f7636ff777a1fd512
Tim Mattison <tim@mattison.org>
2020-02-10 22:26:05 -0500

    Beta 0.8.10 release
    This reverts commit 08b737742e6a1c5d2e31fec20d79b1564e6a0e8c.

--- foundation/CDDBaselineJava/src/main/java/com/awslabs/aws/iot/greengrass/cdd/BaselineAppInjector.java
+++ foundation/CDDBaselineJava/src/main/java/com/awslabs/aws/iot/greengrass/cdd/BaselineAppInjector.java
@@ -4,12 +4,11 @@ import com.awslabs.aws.iot.greengrass.cdd.modules.BaselineAppModule;
 import com.awslabs.aws.iot.greengrass.cdd.providers.interfaces.EnvironmentProvider;
 import com.google.common.eventbus.EventBus;
 import dagger.Component;
-import dagger.Subcomponent;
 
 import javax.inject.Singleton;
 
 @Singleton
-@Subcomponent(modules = BaselineAppModule.class)
+@Component(modules = BaselineAppModule.class)
 public interface BaselineAppInjector {
     EnvironmentProvider environmentProvider();
 
