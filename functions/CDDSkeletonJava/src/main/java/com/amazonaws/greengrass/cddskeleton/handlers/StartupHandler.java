dbb67dd2d34ff59f21cc12109c5714574ac98959
Tim Mattison <tim@mattison.org>
2020-02-11 17:48:23 -0500

    Migrated CDD Skeleton to CDD 0.8.49

--- functions/CDDSkeletonJava/src/main/java/com/amazonaws/greengrass/cddskeleton/handlers/StartupHandler.java
+++ functions/CDDSkeletonJava/src/main/java/com/amazonaws/greengrass/cddskeleton/handlers/StartupHandler.java
@@ -1,19 +1,22 @@
 package com.amazonaws.greengrass.cddskeleton.handlers;
 
 import com.amazonaws.greengrass.cddskeleton.data.Topics;
-import com.awslabs.aws.iot.greengrass.cdd.communication.Communication;
-import com.awslabs.aws.iot.greengrass.cdd.events.GreengrassStartEvent;
+import com.awslabs.aws.iot.greengrass.cdd.communication.Dispatcher;
+import com.awslabs.aws.iot.greengrass.cdd.events.ImmutableGreengrassStartEvent;
 import com.awslabs.aws.iot.greengrass.cdd.handlers.interfaces.GreengrassStartEventHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class StartupHandler implements GreengrassStartEventHandler {
+    private final Logger log = LoggerFactory.getLogger(StartupHandler.class);
     private static final int START_DELAY_MS = 5000;
     private static final int PERIOD_MS = 5000;
     @Inject
-    Communication communication;
+    Dispatcher dispatcher;
     @Inject
     Topics topics;
 
@@ -21,21 +24,26 @@ public class StartupHandler implements GreengrassStartEventHandler {
     public StartupHandler() {
     }
 
+    @Inject
+    public void afterInject() {
+        dispatcher.add(ImmutableGreengrassStartEvent.class, this::execute);
+    }
+
     /**
      * Receives the Greengrass start event from the event bus, publishes a message indicating it has started, and creates
      * a timer that publishes a message every 5 seconds after a 5 second delay
      *
-     * @param greengrassStartEvent
+     * @param immutableGreengrassStartEvent
      */
     @Override
-    public void execute(GreengrassStartEvent greengrassStartEvent) {
-        communication.publishMessageEvent(topics.getOutputTopic(), "Skeleton started [" + System.currentTimeMillis() + "]");
+    public void execute(ImmutableGreengrassStartEvent immutableGreengrassStartEvent) {
+        dispatcher.publishMessageEvent(topics.getOutputTopic(), "Skeleton started [" + System.currentTimeMillis() + "]");
 
         Timer timer = new Timer(true);
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                communication.publishMessageEvent(topics.getOutputTopic(), "Skeleton still running... [" + System.currentTimeMillis() + "]");
+                dispatcher.publishMessageEvent(topics.getOutputTopic(), "Skeleton still running... [" + System.currentTimeMillis() + "]");
             }
         }, START_DELAY_MS, PERIOD_MS);
     }
