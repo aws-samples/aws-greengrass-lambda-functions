4d0107455749f15c915194cca0c55f35a8a4d84d
Tim Mattison <tim@mattison.org>
2020-04-15 14:50:30 -0400

    Updated CDD skeleton

--- functions/CDDSkeletonJava/src/main/java/com/amazonaws/greengrass/cddskeleton/handlers/StartupHandler.java
+++ functions/CDDSkeletonJava/src/main/java/com/amazonaws/greengrass/cddskeleton/handlers/StartupHandler.java
@@ -4,12 +4,12 @@ import com.amazonaws.greengrass.cddskeleton.data.Topics;
 import com.awslabs.aws.iot.greengrass.cdd.communication.Dispatcher;
 import com.awslabs.aws.iot.greengrass.cdd.events.ImmutableGreengrassStartEvent;
 import com.awslabs.aws.iot.greengrass.cdd.handlers.interfaces.GreengrassStartEventHandler;
+import com.awslabs.aws.iot.greengrass.cdd.helpers.TimerHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 public class StartupHandler implements GreengrassStartEventHandler {
     private final Logger log = LoggerFactory.getLogger(StartupHandler.class);
@@ -19,6 +19,8 @@ public class StartupHandler implements GreengrassStartEventHandler {
     Dispatcher dispatcher;
     @Inject
     Topics topics;
+    @Inject
+    TimerHelper timerHelper;
 
     @Inject
     public StartupHandler() {
@@ -34,12 +36,7 @@ public class StartupHandler implements GreengrassStartEventHandler {
     public void execute(ImmutableGreengrassStartEvent immutableGreengrassStartEvent) {
         dispatcher.publishMessageEvent(topics.getOutputTopic(), "Skeleton started [" + System.nanoTime() + "]");
 
-        Timer timer = new Timer(true);
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                dispatcher.publishMessageEvent(topics.getOutputTopic(), "Skeleton still running... [" + System.nanoTime() + "]");
-            }
-        }, START_DELAY_MS, PERIOD_MS);
+        Runnable runnable = () -> dispatcher.publishMessageEvent(topics.getOutputTopic(), "Skeleton still running... [" + System.nanoTime() + "]");
+        timerHelper.scheduleRunnable(runnable, START_DELAY_MS, PERIOD_MS, TimeUnit.MILLISECONDS);
     }
 }
